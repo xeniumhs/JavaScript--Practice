f172e6831a67d53b34a65f07a6179db08ab36dea
xeniumhs <thexenium07@gmail.com>
2025-04-12 16:21:53 +0545

    Added Basics

---
+++ 01.Basics/app.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+console.warn("this is warning");
+console.table({ name: "xen", age: "20" });
+
+// (reserved keyword) (variable name) (type of value)
+
+// Declare Variable
+let age;
+// Asign value 
+let banana;
+banana = 'banana';
+console.log(banana);
+
+
+// Primitive Types
+// 1.Numbers
+let num = 10;
+console.log(num);
+console.log(typeof num);
---
+++ 01.Basics/app1.js
@@ -0,0 +1,16 @@
+// 1.Create variable name(name) & store your name.
+// Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+// Create variable name (gender) & store your gender.
+// Create variable name (twitterHandle) & store your twitter handle.
+// 5. Finally log all variables to the console.
+
+let name = "xenium";
+console.log(name);
+
+let whatDoYouWannaBecomeInYourLife = "programmer";
+
+let gender = "Male";
+let twitterHandle = 'xenhs';
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
