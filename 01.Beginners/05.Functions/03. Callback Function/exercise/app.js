bcfaaffeda93635a4614af1f1ea0cce764f5a762
xeniumhs <thexenium07@gmail.com>
2025-04-13 08:47:35 +0545

    added exercise on callback func

--- 01.Beginners/05.Functions/03. Callback Function/exercise/app.js
+++ 01.Beginners/05.Functions/03. Callback Function/exercise/app.js
@@ -3,3 +3,12 @@
 // 3. inside that function, create variable which holds value of 10
 // 4. pass that value to the callback function
 // 5. Now create callback function & print that value which you pass as parameter to the callback func.
+
+function showCallFunc(fn) {
+    const value = 10;
+    fn(value);
+}
+
+showCallFunc(function (value) {
+    console.log(value);
+})
