8fce0b6db724983c918b76fed2a45282fdfe5b65
xeniumhs <thexenium07@gmail.com>
2025-04-13 11:28:33 +0545

    added scope

---
+++ 01.Beginners/05.Functions/04. Scope/app.js
@@ -0,0 +1,27 @@
+// Scope in JavaScript refers to the current context of code, which determines the accessibility of variables to JavaScript.
+
+// The two types of scope are local and global
+
+// Global variables are those declared outside of a block.
+
+// Local variables are those declared inside of a block.
+// Global Scope
+// Local Scope
+
+let textMessage = "hi"; // Global Scope
+let i = 0;
+
+function showMessage() {
+  let textMessage = "hey"; // Local Scope
+  console.log(textMessage); // Access Local Scope
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(i); // Local Scope
+}
+
+console.log(i); // Global Scope
+
+showMessage();
+console.log(textMessage); // Access Global Scope
+
