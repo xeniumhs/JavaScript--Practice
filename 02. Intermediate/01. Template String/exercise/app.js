a617ebfa88ea45428ae03e605b7055f9531f74f0
xeniumhs <thexenium07@gmail.com>
2025-04-15 08:28:40 +0545

    added exercise of template string

---
+++ 02. Intermediate/01. Template String/exercise/app.js
@@ -0,0 +1,19 @@
+// 1. Store this in template string ~
+// The quick
+// brown fox
+// jumps over
+// the lazy dog
+
+// 2. Create two variables (firstName, lastName)
+// 3. Use that variables in template string.
+
+text = `The quick
+brown fox
+jumps over
+the lazy dog`;
+
+let firstName = "Xen";
+let lastName = "hs";
+
+let text1 = `Welcome ${firstName} ${lastName}`
+console.log(text1);
