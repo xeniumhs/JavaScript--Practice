0f1bdb53cb6bd9079d92e1b06e589bb9fff150ff
xeniumhs <thexenium07@gmail.com>
2025-04-17 10:00:38 +0545

    upto default func parameter codes

---
+++ 02. Intermediate/04. Default function Parameters/codes/app.js
@@ -0,0 +1,23 @@
+// Default function parameters, introduced in ECMAScript 6 (ES6), allow you to assign default values to function parameters. When a function is called, and an argument is not provided for a parameter, the default value will be used instead. This feature makes it easier to handle cases where arguments might be missing or undefined.
+
+function countTo5(count=false) {
+  if (count===true) {
+    for (let i = 0; i <=5 ; i++) {
+      console.log(`Count:${i}`);
+      
+    }
+  }
+}
+
+countTo5(true);
+
+
+function ratings(rate=0) {
+  if (rate === 5) {
+    console.log(`High Ratings:)`);
+  }
+  else if (rate === 0) {
+    console.log(`Low Ratings :(`); }
+}
+
+ratings(5);
---
+++ 02. Intermediate/04. Default function Parameters/exercise/app.js
@@ -0,0 +1,3 @@
+// ---------------------------
+// 1. Create a function name (append) it will take 2 parameters (value, array)
+// 2. The default value of parameter "array" will be [].
