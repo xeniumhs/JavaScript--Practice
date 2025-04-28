93701c220b46ae14acce1a9f01335d890389728a
xeniumhs <thexenium07@gmail.com>
2025-04-28 18:41:44 +0545

    upto ternary operator all

---
+++ 02. Intermediate/08. Ternary Operator/exercise/app.js
@@ -0,0 +1,28 @@
+// -------------------------------------
+// REFACTOR THIS CODE & USE TERNARY OPERATOR
+// check the age to determine the eligibility to vote
+// let age = 15;
+// let result;
+
+// if (age >= 18) {
+//       result = "You are eligible to vote.";
+// } else {
+//       result = "You are not eligible to vote yet.";
+// }
+
+// console.log(result);
+// -------------------------------------
+
+// -------------------------------------
+// ternary operator to check the eligibility to vote
+
+let age = 15;
+let result = age >= 18 ? "Eligible to vote" : "not eligible to vote"
+console.log(result);
+
+// -------------------------------------
+// Create a program which checks if number is positive, negative or zero
+
+let a = -3;
+let res = a >= 0 ? (a > 0 ? "positive" : "zero") : "negative";
+console.log(`The number is ${res}`);
