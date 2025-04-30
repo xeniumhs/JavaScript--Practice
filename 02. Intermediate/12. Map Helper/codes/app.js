12ae78afe31432f5c29a84e1d1dfff92d6f782e9
xeniumhs <thexenium07@gmail.com>
2025-04-30 21:44:32 +0545

    upto mapHelper code

---
+++ 02. Intermediate/12. Map Helper/codes/app.js
@@ -0,0 +1,22 @@
+// The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+
+let numbers = [1, 2, 3, 4, 5];
+
+// So here what's going on, each number in the numbers array is being passed
+// --> Into anonymous function that we're passing in,
+// --> Whatever this function returns is placed into a new array,
+// --> After each elements has being processed that new array is return.
+let double = numbers.map((num) => num * 2);
+console.log(double);
+// -----------------
+
+let peoples = [
+  { firstName: "Xen", lastName: "HS" },
+  { firstName: "zen", lastName: "noe" },
+  { firstName: "Xoy", lastName: "wq" },
+];
+
+const results = peoples.map((person) => {
+  return [person.firstName, person.lastName];
+});
+console.log(results);
