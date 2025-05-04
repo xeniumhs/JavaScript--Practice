c3aa548fca5981ef094f93f5f301316c67894616
xeniumhs <thexenium07@gmail.com>
2025-05-04 18:29:30 +0545

    Upto codes od every and some helper

--- 02. Intermediate/14. Every and Some Helpers/codes/app.js
+++ 02. Intermediate/14. Every and Some Helpers/codes/app.js
@@ -14,3 +14,18 @@ const res2 = peoples.some((people) => people.length < 3);
 
 console.log(res);
 console.log(res2);
+
+
+//////////////////////////////////////////
+const songs = [
+  { name: "Lucky You", duration: 4.34 },
+  { name: "Just Like You", duration: 3.23 },
+  { name: "The Search", duration: 2.33 },
+  { name: "Old Town Road", duration: 1.43 },
+  { name: "The Box", duration: 5.23 },
+];
+
+const everyRes = songs.every((song) => song.duration > 3);
+const someRes = songs.some((song) => song.duration > 3);
+console.log(everyRes);
+console.log(someRes);
